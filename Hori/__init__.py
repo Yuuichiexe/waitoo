43a34f0dd56342c00e1b0095cd8eb3d21928d439
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-06-21 19:17:16 +0530

    Add files via upload

---
+++ Hori/__init__.py
@@ -0,0 +1,361 @@
+import logging
+import os
+import sys
+import time
+
+import aiohttp
+import httpx
+import spamwatch
+import pymongo
+import telegram.ext as tg
+from redis import StrictRedis
+from aiohttp import ClientSession
+from httpx import AsyncClient, Timeout
+from pymongo import MongoClient 
+from odmantic import AIOEngine
+from Hori.confing import get_int_key, get_str_key
+from motor import motor_asyncio
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, PeerIdInvalid
+from Python_ARQ import ARQ
+from telegraph import Telegraph
+from telethon import TelegramClient
+from telethon.sessions import MemorySession, StringSession
+
+StartTime = time.time()
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+# if version < 3.6, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.",
+    )
+    sys.exit(1)
+
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+    JOIN_LOGGER = os.environ.get("JOIN_LOGGER", None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        DRAGONS = {int(x) for x in os.environ.get("DRAGONS", "").split()}
+        DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")
+
+    try:
+        DEMONS = {int(x) for x in os.environ.get("DEMONS", "").split()}
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WOLVES = {int(x) for x in os.environ.get("WOLVES", "").split()}
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    try:
+        TIGERS = {int(x) for x in os.environ.get("TIGERS", "").split()}
+    except ValueError:
+        raise Exception("Your scout users list does not contain valid integers.")
+
+    INFOPIC = bool(
+        os.environ.get("INFOPIC", True)
+    )  # Info Pic (use True[Value] If You Want To Show In /info.)
+    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    ARQ_API_URL = os.environ.get("ARQ_API_URL", None)
+    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")  # Bot Username
+    BOT_NAME = os.environ.get("BOT_NAME", "")  # Bot Name
+    ERROR_LOGS = os.environ.get(
+        "ERROR_LOGS", None
+    )  # Error Logs (Channel Or Group Choice Is Yours)
+    URL = os.environ.get("URL", "")  # Does not contain token
+    PORT = int(os.environ.get("PORT", 5000))
+    CERT_PATH = os.environ.get("CERT_PATH")
+    API_ID = os.environ.get("API_ID", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    DONATION_LINK = os.environ.get("DONATION_LINK")
+    LOAD = os.environ.get("LOAD", "").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    WORKERS = int(os.environ.get("WORKERS", 8))
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+    AI_API_KEY = os.environ.get("AI_API_KEY", None)
+    WALL_API = os.environ.get("WALL_API", None)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
+    STRING_SESSION = os.environ.get(
+        "STRING_SESSION"
+    )  # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
+    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
+    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+    REPOSITORY = os.environ.get("REPOSITORY", "")
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "lightYagami")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    HELP_IMG = os.environ.get("HELP_IMG", True)
+    REDIS_URL = os.environ.get("REDIS_URL", None) # REDIS URL (From:- Heraku & Redis)
+    OPENWEATHERMAP_ID = os.environ.get(
+        "OPENWEATHERMAP_ID", ""
+    )  # From:- https://openweathermap.org/api
+    LOG_GROUP_ID = os.environ.get("LOG_GROUP_ID", None)
+    STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
+    MONGO_DB = "Shikimori"
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    REM_BG_API_KEY = os.environ.get(
+        "REM_BG_API_KEY", None
+    )  # From:- https://www.remove.bg/
+    try:
+        BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+else:
+    from Hori.config import Development as Config
+
+    TOKEN = Config.TOKEN
+
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("Your OWNER_ID variable is not a valid integer.")
+
+    JOIN_LOGGER = Config.JOIN_LOGGER
+    OWNER_USERNAME = Config.OWNER_USERNAME
+
+    try:
+        DRAGONS = {int(x) for x in Config.DRAGONS or []}
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
+    except ValueError:
+        raise Exception("Your sudo or dev users list does not contain valid integers.")
+
+    try:
+        DEMONS = {int(x) for x in Config.DEMONS or []}
+    except ValueError:
+        raise Exception("Your support users list does not contain valid integers.")
+
+    try:
+        WOLVES = {int(x) for x in Config.WOLVES or []}
+    except ValueError:
+        raise Exception("Your whitelisted users list does not contain valid integers.")
+
+    try:
+        TIGERS = {int(x) for x in Config.TIGERS or []}
+    except ValueError:
+        raise Exception("Your tiger users list does not contain valid integers.")
+
+    EVENT_LOGS = Config.EVENT_LOGS
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    CERT_PATH = Config.CERT_PATH
+    API_ID = Config.API_ID
+    API_HASH = Config.API_HASH
+    STRING_SESSION = Config.STRING_SESSION
+    DB_URI = Config.SQLALCHEMY_DATABASE_URI
+    REDIS_URL = Config.REDIS_URL
+    DONATION_LINK = Config.DONATION_LINK
+    LOAD = Config.LOAD
+    HELP_IMG = Config.HELP_IMG
+    NO_LOAD = Config.NO_LOAD
+    ERROR_LOGS = Config.ERROR_LOGS
+    DEL_CMDS = Config.DEL_CMDS
+    MONGO_DB = Config.MONGO_DB
+    MONGO_DB_URI = Config.MONGO_DB_URI
+    STRICT_GBAN = Config.STRICT_GBAN
+    WORKERS = Config.WORKERS
+    BAN_STICKER = Config.BAN_STICKER
+    ALLOW_EXCL = Config.ALLOW_EXCL
+    CASH_API_KEY = Config.CASH_API_KEY
+    TIME_API_KEY = Config.TIME_API_KEY
+    AI_API_KEY = Config.AI_API_KEY
+    WALL_API = Config.WALL_API
+    SUPPORT_CHAT = Config.SUPPORT_CHAT
+    SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
+    SPAMWATCH_API = Config.SPAMWATCH_API
+    INFOPIC = Config.INFOPIC
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    ARQ_API_URL = "arq.hamker.dev"
+    ARQ_API_KEY = "UMPYGF-MVNLVW-RTNXKA-FJWOUH-ARQ"
+
+    OPENWEATHERMAP_ID = "ca1f9caacbb92187db96c0bf5686017b"
+
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
+
+    try:
+        BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
+    except ValueError:
+        raise Exception("Your blacklisted chats list does not contain valid integers.")
+
+
+DEV_USERS.add(1143358497)
+REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+REDIS_URL = "redis://default:6vsajwd6wq3lNI7T3zMIpltTrU040DJC@redis-19248.c16.us-east-1-3.ec2.cloud.redislabs.com:19248"
+
+try:
+    REDIS.ping()
+    LOGGER.info("Your redis server is now alive!")
+
+except BaseException:
+    raise Exception("Your redis server is not alive, please check again.")
+
+finally:
+   REDIS.ping()
+   LOGGER.info("Your redis server is now alive!")
+
+if not SPAMWATCH_API:
+    sw = None
+    LOGGER.warning("SpamWatch API key missing! recheck your config.")
+else:
+    sw = spamwatch.Client(SPAMWATCH_API)
+
+
+session_name = TOKEN.split(":")[0]
+pgram = Client(
+    session_name,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+)
+
+# Credits Logger
+print(
+    "[Hori] Horix Is Starting. | Programmer Network Project | BSD 2-Clause License."
+)
+print(
+    "[Hori] Mewo Mewo! Successfully Connected With Programmer • Data Center • Chennai"
+)
+print(
+    "[Hori] Project Maintained By: github.com/Eren-YeagerO (https://github.com/Eren-YeagerO/Hori)"
+)
+
+mongodb = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+db = motor[MONGO_DB]
+engine = AIOEngine(motor, MONGO_DB)
+
+print("[Hori]: Telegraph Installing")
+telegraph = Telegraph()
+print("[Hori]: Telegraph Account Creating")
+telegraph.create_account(short_name="Horix")
+updater = tg.Updater(
+    token=TOKEN,
+    workers=WORKERS,
+    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
+    use_context=True,
+)
+print("[Hori]: TELETHON CLIENT STARTING")
+tbot = TelegramClient(MemorySession(), API_ID, API_HASH)
+dispatcher = updater.dispatcher
+
+BOT_ID = dispatcher.bot.id
+BOT_NAME = dispatcher.bot.first_name
+BOT_USERNAME = dispatcher.bot.username
+# asyncio.get_event_loop().run_until_complete(dispatcher.bot.initialize())
+# ------------------------------------------------------------------
+print("[Hori]: PYROGRAM CLIENT STARTING")
+PyroGram = TOKEN.split(":")[0]
+pgram = Client(
+    name=PyroGram,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+    parse_mode=ParseMode.HTML,
+    sleep_threshold=60,
+    in_memory=True,
+)
+print("[INFO]: INITIALZING AIOHTTP SESSION")
+aiohttpsession = ClientSession()
+# ARQ Client
+print("[INFO]: INITIALIZING ARQ CLIENT")
+arq = ARQ("https://thearq.tech", "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ", aiohttpsession)
+print(
+    "[Hori]: Connecting To Programmer • Data Center • Chennai • PostgreSQL Database"
+)
+ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+print(
+    "[Hori]: Connecting To Programmer • Horix Userbot (https://telegram.dog/Awesome_Hori)"
+)
+timeout = httpx.Timeout(40)
+http = httpx.AsyncClient(http2=True, timeout=timeout)
+
+
+async def get_entity(client, entity):
+    entity_client = client
+    if not isinstance(entity, Chat):
+        try:
+            entity = int(entity)
+        except ValueError:
+            pass
+        except TypeError:
+            entity = entity.id
+        try:
+            entity = await client.get_chat(entity)
+        except (PeerIdInvalid, ChannelInvalid):
+            for pgram in apps:
+                if pgram != client:
+                    try:
+                        entity = await pgram.get_chat(entity)
+                    except (PeerIdInvalid, ChannelInvalid):
+                        pass
+                    else:
+                        entity_client = pgram
+                        break
+            else:
+                entity = await pgram.get_chat(entity)
+                entity_client = pgram
+    return entity, entity_client
+
+
+apps = [pgram]
+
+DRAGONS = list(DRAGONS) + list(DEV_USERS)
+DEV_USERS = list(DEV_USERS)
+WOLVES = list(WOLVES)
+DEMONS = list(DEMONS)
+TIGERS = list(TIGERS)
+
+# Load at end to ensure all prev variables have been set
+from Hori.modules.helper_funcs.handlers import (
+    CustomCommandHandler,
+    CustomMessageHandler,
+    CustomRegexHandler,
+)
+
+# make sure the regex handler can take extra kwargs
+tg.RegexHandler = CustomRegexHandler
+tg.CommandHandler = CustomCommandHandler
+tg.MessageHandler = CustomMessageHandler
+
+LOGGER.info("[Hori IS READY]")
